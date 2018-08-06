0740e604137ea145da30310eddd1c9f1ac878e7d
Sixl-Daniel <sixl.daniel@googlemail.com>
2018-08-06 14:32:08 +0200

    feat: Add link to local host

--- serve.bat
+++ serve.bat
@@ -1,9 +1,16 @@
 @echo off
-
+color 03
+cls
 echo Starting python server!
+echo.
+echo Alt+Click to follow link:
 
 if "%1" == "" (
+    echo http://localhost:8000/
+    echo.
     python -m SimpleHTTPServer 8000
 ) else (
+    echo http://localhost:%1/
+    echo.
     python -m SimpleHTTPServer %1
 )
